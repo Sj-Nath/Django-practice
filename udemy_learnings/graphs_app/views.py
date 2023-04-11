ea1793f9edd7ae9d0b4e15a5fccade4f51e2e6d3
Subhrajyoti Nath <subhrajyotinath.98@gmail.com>
2023-04-11 14:55:54 +0530

    Showing content from model like a table
    Showing content from model like a table

--- udemy_learnings/graphs_app/views.py
+++ udemy_learnings/graphs_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from graphs_app.models import Topic,Webpage,AccessRecord
 # Create your views here.
 
 
 def home(request):
+    webpage_list = AccessRecord.objects.order_by('date')
+
+
     home_dict={
-        'graph1':'Here is your Graph'
+        'graph1':'Here is your Table',
+        'access_records' : webpage_list
     }
     return render(request,'graphs_app/home.html',home_dict)
